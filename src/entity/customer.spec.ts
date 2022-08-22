faf5f46fb5f7811ef91f044ce750949d1d4e48b1
Leomara <leomara.andrade@gmail.com>
2022-08-22 08:28:16 -0300

    TsLint

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -5,37 +5,37 @@ describe('Customer unit test', () => {
     it('should throw error when id is empty', () => {
         expect(() => {
             const customer = new Customer('', 'Customer 1');
-        }).toThrowError('Id is required')
-    })
+        }).toThrowError('Id is required');
+    });
 
     it('should throw error when name is empty', () => {
         expect(() => {
             const customer = new Customer('123', '');
-        }).toThrowError('Name is required')
-    })
+        }).toThrowError('Name is required');
+    });
 
     it('should change name', () => {
         const customer = new Customer('123', 'John');
-        customer.changeName('Anna')
-        expect(customer.name).toBe('Anna')
-    })
+        customer.changeName('Anna');
+        expect(customer.name).toBe('Anna');
+    });
 
     it('should activate customer', () => {
         const customer = new Customer('1', 'Customer 1');
-        const address = new Address('Rua 123', 123, '123521-000', 'SP')
-        customer.changeAddress(address)
-        customer.activate()
-        expect(customer.isActive()).toBe(true)
-    })
+        const address = new Address('Rua 123', 123, '123521-000', 'SP');
+        customer.changeAddress(address);
+        customer.activate();
+        expect(customer.isActive()).toBe(true);
+    });
 
     it('should deactivate customer', () => {
         const customer = new Customer('1', 'Customer 1');
-        customer.deactivate()
-        expect(customer.isActive()).toBe(false)
-    })
+        customer.deactivate();
+        expect(customer.isActive()).toBe(false);
+    });
 
     it('should throw error if address is undefined when activate a customer', () => {
         const customer = new Customer('1', 'Customer 1');
-        expect(() => customer.activate()).toThrowError('Address is mandatory to activate a customer')
-    })
-})
+        expect(() => customer.activate()).toThrowError('Address is mandatory to activate a customer');
+    });
+});
